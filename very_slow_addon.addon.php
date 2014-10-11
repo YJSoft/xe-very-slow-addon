9868ce104fa7a973479c7824fdd7e5f64f2d9a25
YJSoft <yjsoft@yjsoft.pe.kr>
2014-10-11 16:50:49 +0900

    admin 모듈에서 동작하지 않도록 수정

--- very_slow_addon.addon.php
+++ very_slow_addon.addon.php
@@ -1,6 +1,6 @@
 <?php
 if(!defined('__XE__')) exit();
-if($called_position == 'after_module_proc' && (Context::getResponseMethod() == "HTML" || !isCrawler()))
+if($called_position == 'after_module_proc' && (Context::getResponseMethod() == "HTML" || !isCrawler()) && Context::get('module') != 'admin')
 {
 	sleep(rand(1,4));
 }
